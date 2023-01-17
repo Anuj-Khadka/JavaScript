98ea21cf8f08a7c1431d1bb5a2fc745ff5e6d6dd
Anuj-Khadka <anujkhadka008@gmail.com>
2023-01-17 17:51:20 +0545

    digital clock project

--- Projects/Digital Clock/js/script.js
+++ Projects/Digital Clock/js/script.js
@@ -1,1 +1,37 @@
-// cretate a digital clock using setinteral and date object
+// cretate a digital clock using setinteral and date object.
+
+let hourTime = document.getElementById("hour-time");
+let minuteTime = document.getElementById("minute-time");
+let secondTime = document.getElementById("second-time");
+
+let convertedHour;
+
+let time = function(){
+    let date = new Date();
+    dateHour = date.getHours();
+    dateMinute = date.getMinutes();
+    dateSecond = date.getSeconds();
+    if (dateSecond < 10) {
+        secondTime.textContent = `0${dateSecond}`;
+    } else{
+        secondTime.textContent = dateSecond;
+    }
+    if (dateMinute < 10) {
+        minuteTime.textContent = `0${dateMinute}`;
+    } else{
+        minuteTime.textContent = dateMinute;
+    }
+    if(dateHour > 12) {
+        convertedHour = dateHour - 12;
+        hourTime.textContent = convertedHour;
+    }
+    if (dateHour < 10) {
+        hourTime.textContent = `0${dateHour}`;
+    }
+    if (convertedHour < 10) {
+        hourTime.textContent = `0${convertedHour}`;
+    }
+}
+
+
+let setTime = setInterval(time, 1000);
