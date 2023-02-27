4db87b588e001edd6ed2b72e061cf8d48d5f3a5a
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-27 16:46:01 +0545

    update note

---
+++ js/html_insertion.js
@@ -0,0 +1,36 @@
+let contain = document.getElementById("container")
+console.log(contain)
+
+let newDiv = document.createElement('div')
+let newDiv1 = document.createTextNode('dvnisv')
+
+newDiv.innerHTML = `<h1>this is an innerhtml</h1>`
+// contain.append(newDiv)
+// contain.prepend(newDiv)
+// contain.before(newDiv)
+// contain.after(newDiv)
+contain.replaceWith(newDiv)
+
+
+
+
+
+// What is a Web Server? (NGINX)
+
+// https://www.nginx.com/resources/glossary/web-server/
+
+// What is a Web Browser? (Mozilla)
+
+// https://www.mozilla.org/en-US/firefox/browsers/what-is-a-browser/
+
+// Who invented the Internet? And why? (Kurzgesagt) 
+
+// https://youtu.be/21eFwbb48sE
+
+// What is Cloud Computing? (Amazon)
+
+// https://youtu.be/mxT233EdY5c
+
+// Browser Engines (Wikipedia)
+
+// https://en.wikipedia.org/wiki/Browser_engine
