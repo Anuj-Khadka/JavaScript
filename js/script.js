8746d717efd8f15312ad240a203b36ddfff472c2
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-27 16:46:34 +0545

    update note

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// console.log("hello world");
+
+// // this is an object Array
+
+// newarr = [
+//     {
+//         name: "Anuj",
+//         class: "undergraduate",
+//         age: 22
+//     },
+//     {
+//         name: "algae",
+//         class: "high school",
+//         age: 15
+//     },
+//     {
+//         name: "Brutpull",
+//         class: "graduate",
+//         age: 26
+//     }
+// ]
+
+// let stu = newarr.filter(stu => stu.class === "undergraduate")
+// console.log(stu)
+
+
+// Array 
+
+array = ["anuj", "anugya", "tek", "gopi"]
