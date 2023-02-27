77db78691c9a96d11ab5ebdcba895c5e33e1a0ed
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-27 16:45:48 +0545

    update note

---
+++ js/dom_walking.js
@@ -0,0 +1,7 @@
+let body = document.body
+let DocElemen = document.documentElement
+let title = document.title
+let head = document.head
+
+
+console.log(`Body: ${body}\n document element: ${DocElemen}\n title: ${title}\n head: ${head}`)
