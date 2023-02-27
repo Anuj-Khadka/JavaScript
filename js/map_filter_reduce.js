2162c81165c84d0a34a2feeebcccba8359eed928
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-27 16:46:16 +0545

    update note

---
+++ js/map_filter_reduce.js
@@ -0,0 +1,15 @@
+let arr = [3, 5, 6, 8, 9]
+
+// map 
+// let mArr = arr.map((value)=> {return value + 2} )
+// console.log(mArr)
+
+
+//filter
+// let fArr = arr.filter( val => val > 6)
+// console.log(fArr)
+
+
+// reduce 
+let rArr = arr.reduce( (val1, val2) => {return val1 + val2})
+console.log(rArr)
